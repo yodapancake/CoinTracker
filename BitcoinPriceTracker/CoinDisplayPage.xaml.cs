65dd987293554daeea2a7c53121177e0001dee23
yodapancake <maximushirsch@gmail.com>
2018-05-24 13:52:05 -0700

    u
    t

---
+++ BitcoinPriceTracker/CoinDisplayPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using BitcoinPriceTracker.ViewModels;
+using BitcoinPriceTracker.Models;
+using Windows.Storage;
+using System.Collections.ObjectModel;
+using Windows.UI;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BitcoinPriceTracker
+{
+    public sealed partial class CoinDisplayPage : Page
+    {
+        private GlobalDataViewModel Global_Data_viewmodels = new GlobalDataViewModel();
+        private ObservableCollection<TopTenViewModel> Top_ten_viewmodels = new ObservableCollection<TopTenViewModel>();
+        public CoinDisplayPage()
+        {
+            this.InitializeComponent();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await GetInfo();
+        }
+        private async Task GetInfo()
+        {
+            Top10CoinsDetailsRetriever details = new Top10CoinsDetailsRetriever();
+            Top10RootObject Top10Root = await details.GetTop10Details();
+
+            GlobalDetailsRetriever GlobalDetails = new GlobalDetailsRetriever();
+            GlobalData.GlobalDataRootObject GlobalDataRoot = await GlobalDetails.GetGlobalDataDetails();
+
+            Global_Data_viewmodels.Active_Cryptocurrencies = GlobalDataRoot.data.active_cryptocurrencies;
+            Global_Data_viewmodels.Active_Markets = GlobalDataRoot.data.active_markets;
+            Global_Data_viewmodels.Bitcoin_Percentage_Of_Market_Cap = GlobalDataRoot.data.bitcoin_percentage_of_market_cap;
+            Global_Data_viewmodels.Total_Market_Cap = GlobalDataRoot.data.quotes.USD.total_market_cap;
+            Global_Data_viewmodels.Total_Volume_24h = GlobalDataRoot.data.quotes.USD.total_volume_24h;
+
+            foreach (var v in Top10Root.data)
+            {
+                TopTenViewModel temp = new TopTenViewModel();
+                temp.Coin_Name = v.Value.name;
+                temp.Coin_Rank = v.Value.rank;
+                temp.Coin_Ticker_Tape = v.Value.symbol;
+                temp.Coin_Price_USD = v.Value.quotes.USD.price;
+                temp.Coin_24_Hour_Change = v.Value.quotes.USD.percent_change_24h;
+                temp.Coin_24_Hour_Volume = v.Value.quotes.USD.volume_24h;
+                temp.Coin_Circulating_Supply = v.Value.total_supply;
+                temp.Coin_Market_Cap = v.Value.quotes.USD.market_cap;
+                temp.Coin_Picture = "ms-appx:///Assets/CryptoSVG/PNG/" + temp.Coin_Ticker_Tape + ".png";
+
+                temp.Coin_CMC_String = " $" + String.Format("{0:#,##0.##}", temp.Coin_Market_Cap);
+                temp.Coin_Circ_String = " " + String.Format("{0:#,##0.##}", temp.Coin_Circulating_Supply);
+                temp.Coin_Volume_String = "$" + String.Format("{0:#,##0.##}", temp.Coin_24_Hour_Volume);
+
+                double Coin_Market_Share_Percent = ((double)(temp.Coin_Market_Cap) / (long)(Global_Data_viewmodels.Total_Market_Cap));
+                int scale = (int)((Coin_Market_Share_Percent / Global_Data_viewmodels.Bitcoin_Percentage_Of_Market_Cap) * 65000);
+                temp.Coin_Picture_Scale = (int)(Math.Sqrt(scale) * 13);
+
+                if (temp.Coin_24_Hour_Change < 0)
+                {
+                    temp.Coin_Sign = new SolidColorBrush(Color.FromArgb(230, 201, 19, 19));
+                }
+                else
+                {
+                    temp.Coin_Sign = new SolidColorBrush(Color.FromArgb(230, 19, 201, 31));
+                }
+
+                Top_ten_viewmodels.Add(temp);
+            }
+        }
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+        public void Flyout_menu_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }   
+        private void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+
+        }
+    }
+}
--- BitcoinPriceTracker/obj/x64/Debug/XamlTypeInfo.g.cs
+++ BitcoinPriceTracker/obj/x64/Debug/XamlTypeInfo.g.cs
@@ -132,15 +132,17 @@ namespace BitcoinPriceTracker.BitcoinPriceTracker_XamlTypeInfo
 
         private void InitTypeTables()
         {
-            _typeNameTable = new string[3];
-            _typeNameTable[0] = "BitcoinPriceTracker.MainPage";
+            _typeNameTable = new string[4];
+            _typeNameTable[0] = "BitcoinPriceTracker.CoinDisplayPage";
             _typeNameTable[1] = "Windows.UI.Xaml.Controls.Page";
             _typeNameTable[2] = "Windows.UI.Xaml.Controls.UserControl";
+            _typeNameTable[3] = "BitcoinPriceTracker.MainPage";
 
-            _typeTable = new global::System.Type[3];
-            _typeTable[0] = typeof(global::BitcoinPriceTracker.MainPage);
+            _typeTable = new global::System.Type[4];
+            _typeTable[0] = typeof(global::BitcoinPriceTracker.CoinDisplayPage);
             _typeTable[1] = typeof(global::Windows.UI.Xaml.Controls.Page);
             _typeTable[2] = typeof(global::Windows.UI.Xaml.Controls.UserControl);
+            _typeTable[3] = typeof(global::BitcoinPriceTracker.MainPage);
         }
 
         private int LookupTypeIndexByName(string typeName)
@@ -175,7 +177,8 @@ namespace BitcoinPriceTracker.BitcoinPriceTracker_XamlTypeInfo
             return -1;
         }
 
-        private object Activate_0_MainPage() { return new global::BitcoinPriceTracker.MainPage(); }
+        private object Activate_0_CoinDisplayPage() { return new global::BitcoinPriceTracker.CoinDisplayPage(); }
+        private object Activate_3_MainPage() { return new global::BitcoinPriceTracker.MainPage(); }
 
         private global::Windows.UI.Xaml.Markup.IXamlType CreateXamlType(int typeIndex)
         {
@@ -187,9 +190,9 @@ namespace BitcoinPriceTracker.BitcoinPriceTracker_XamlTypeInfo
             switch (typeIndex)
             {
 
-            case 0:   //  BitcoinPriceTracker.MainPage
+            case 0:   //  BitcoinPriceTracker.CoinDisplayPage
                 userType = new global::BitcoinPriceTracker.BitcoinPriceTracker_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
-                userType.Activator = Activate_0_MainPage;
+                userType.Activator = Activate_0_CoinDisplayPage;
                 userType.SetIsLocalType();
                 xamlType = userType;
                 break;
@@ -200,6 +203,13 @@ namespace BitcoinPriceTracker.BitcoinPriceTracker_XamlTypeInfo
 
             case 2:   //  Windows.UI.Xaml.Controls.UserControl
                 xamlType = new global::BitcoinPriceTracker.BitcoinPriceTracker_XamlTypeInfo.XamlSystemBaseType(typeName, type);
+                break;
+
+            case 3:   //  BitcoinPriceTracker.MainPage
+                userType = new global::BitcoinPriceTracker.BitcoinPriceTracker_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
+                userType.Activator = Activate_3_MainPage;
+                userType.SetIsLocalType();
+                xamlType = userType;
                 break;
             }
             return xamlType;
